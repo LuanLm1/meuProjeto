248ace6213b46e3f7f375cc009463cb94a1ae2e5
Luan Lagrimante Martinho <llagrimante2@gmail.com>
2023-06-25 15:35:35 -0300

    Calcula a diferença de horas

--- projetoPlanejar/calcular.php
+++ projetoPlanejar/calcular.php
@@ -1,6 +1,8 @@
 <?php
-
     $dataHoraInicial = strtotime($_POST["data_hora_inicial"]);
     $dataHoraFinal = strtotime($_POST["data_hora_final"]); 
 
+    $diferencaHoras = $dataHoraFinal - $dataHoraInicial;
+    $horasTrabalhadas = round($diferencaHoras / 3600, 2);
+    
 ?>
