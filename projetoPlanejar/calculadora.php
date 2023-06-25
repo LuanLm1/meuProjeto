fc426919256daa2ccc2234bb4b21155d6107327e
Luan Lagrimante Martinho <llagrimante2@gmail.com>
2023-06-25 11:22:13 -0300

    Formulário
    Verificar se o formulário foi enviado e logo em seguida vamos obter os valores do formulário

---
+++ projetoPlanejar/calculadora.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>projetoPlanejar</title>
+</head>
+<body>
+
+<?php 
+    if ($_SERVER["REQUEST_METHOD"] == "POST") {
+
+        $dataInicial = $_POST["data_inicial"];
+        $horaInicial = $_POST["hora_inicial"];
+        $dataFinal = $_POST["data_final"];
+        $horaFinal = $_POST["hora_final"];
+    }
+?>
+</body>
+</html>
